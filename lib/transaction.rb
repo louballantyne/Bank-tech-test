e7f1075d23d0c49c366959aa726d9ccb10ba61a7
Louise Ballantyne <lousballantyne@icloud.com>
2021-05-04 13:54:46 +0100

    A transaction stores a debit and credit correctly

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,6 +1,8 @@
 class Transaction
-  attr_reader :date
-  def initialize
+  attr_reader :date, :debit, :credit
+  def initialize(credit: 0, debit: 0)
     @date = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    @debit = debit
+    @credit = credit
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,10 +1,17 @@
 require 'transaction'
 
 describe Transaction do
+  alias_method :transaction, :subject
+
   describe 'a transaction' do
     it 'has a date' do
       transaction = Transaction.new
       expect(transaction.date).to eq Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    end
+    it 'creates a transaction with a debit and no credit' do
+      transaction2 = Transaction.new(debit: 4)
+      expect(transaction2.debit).to eq 4
+      expect(transaction2.credit).to eq 0
     end
   end
 end
