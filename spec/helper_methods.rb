9acff226858f3cc41301bebe153905029da95939
Louise Ballantyne <lousballantyne@icloud.com>
2021-05-04 19:42:34 +0100

    Refactored statement spec to stub out methods for tests with 1 transaction

--- spec/helper_methods.rb
+++ spec/helper_methods.rb
@@ -1,34 +1,19 @@
-def bank_with_deposit
-  bank = Bank.new
-  bank.instance_variable_set(:@balance, 1000)
-  bank.instance_variable_set(:@statement, statement)
-  bank.deposit(500)
+def stubbed_transactions_1_deposit
+  transaction = double("transaction", :date => Time.now.strftime("%Y-%m-%d"), :debit => 0, :credit => 500, :balance => 1500)
+  transactions = [transaction]
+  statement.instance_variable_set(:@transactions, transactions)
 end
 
-def bank_with_withdrawal
-  bank = Bank.new
-  bank.instance_variable_set(:@balance, 1000)
-  bank.instance_variable_set(:@statement, statement)
-  bank.withdraw(800)
+def stubbed_transactions_1_withdrawal
+  transaction = double("transaction", :date => Time.now.strftime("%Y-%m-%d"), :debit => 800, :credit => 0, :balance => 200)
+  transactions = [transaction]
+  statement.instance_variable_set(:@transactions, transactions)
 end
 
-def bank_with_3_transactions
-  bank = Bank.new
-  bank.instance_variable_set(:@balance, 1000)
-  bank.instance_variable_set(:@statement, statement)
-  bank.withdraw(800)
-  bank.deposit(12000)
-  bank.withdraw(5000)
-end
-
-def stubbed_bank_3_transactions
+def stubbed_transactions_3
   transaction1 = double("transaction", :date => Time.now.strftime("%Y-%m-%d"), :debit => 800, :credit => 0, :balance => 200)
   transaction2 = double("transaction", :date => Time.now.strftime("%Y-%m-%d"), :debit => 0, :credit => 12000, :balance => 12200)
   transaction3 = double("transaction", :date => Time.now.strftime("%Y-%m-%d"), :debit => 5000, :credit => 0, :balance => 7200)
-  bank = double('bank', :balance => 1000, :statement => statement)
-  allow(bank).to receive(:withdraw).with(800).and_return(200)
-  allow(bank).to receive(:deposit).with(12000).and_return(12200)
-  allow(bank).to receive(:withdraw).with(5000).and_return(7200)
   transactions = [transaction1, transaction2, transaction3]
   statement.instance_variable_set(:@transactions, transactions)
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -14,35 +14,36 @@ describe Statement do
 
   context 'when there has been one transaction with a credit' do
     it 'prints a statement containing a date from a previous deposit' do
-      bank_with_deposit
+      stubbed_transactions_1_deposit
       expect(statement.print_statement).to include "#{Time.now.strftime("%Y-%m-%d")}"
     end
     it 'prints a statement containing a credit from a previous deposit' do
-      bank_with_deposit
+      stubbed_transactions_1_deposit
       expect(statement.print_statement).to include "500"
     end
     it 'prints a statement containing the balance following a previous deposit' do
-      bank_with_deposit
+      stubbed_transactions_1_deposit
       expect(statement.print_statement).to include "1500"
     end
     it 'correctly prints a full statement' do
-      bank_with_deposit
+      stubbed_transactions_1_deposit
       expect(statement.print_statement).to eq "date || credit || debit || balance\n#{Time.now.strftime("%Y-%m-%d")} || 500.00 || || 1500.00"
     end
   end
+
   context 'when there has been one transaction with a debit' do
     it 'prints a statement containing a debit from a previous deposit' do
-      bank_with_withdrawal
+      stubbed_transactions_1_withdrawal
       expect(statement.print_statement).to include "800"
     end
     it 'correctly prints a full statement' do
-      bank_with_withdrawal
+      stubbed_transactions_1_withdrawal
       expect(statement.print_statement).to eq "date || credit || debit || balance\n#{Time.now.strftime("%Y-%m-%d")} || || 800.00 || 200.00"
     end
   end
   context 'when there have been three transactions' do
     it 'correctly prints a statement containing all transactions (stubbed)' do
-      stubbed_bank_3_transactions
+      stubbed_transactions_3
       expect(statement.print_statement).to eq "date || credit || debit || balance\n#{Time.now.strftime("%Y-%m-%d")} || || 800.00 || 200.00\n#{Time.now.strftime("%Y-%m-%d")} || 12000.00 || || 12200.00\n#{Time.now.strftime("%Y-%m-%d")} || || 5000.00 || 7200.00"
     end
   end
