53cc928109eb2d9dccc8e5b83ebd7bd8788b218f
Louise Ballantyne <lousballantyne@icloud.com>
2021-05-04 19:36:57 +0100

    Refactored statement spec to stub out methods for test with 3 transactions

--- spec/helper_methods.rb
+++ spec/helper_methods.rb
@@ -20,3 +20,15 @@ def bank_with_3_transactions
   bank.deposit(12000)
   bank.withdraw(5000)
 end
+
+def stubbed_bank_3_transactions
+  transaction1 = double("transaction", :date => Time.now.strftime("%Y-%m-%d"), :debit => 800, :credit => 0, :balance => 200)
+  transaction2 = double("transaction", :date => Time.now.strftime("%Y-%m-%d"), :debit => 0, :credit => 12000, :balance => 12200)
+  transaction3 = double("transaction", :date => Time.now.strftime("%Y-%m-%d"), :debit => 5000, :credit => 0, :balance => 7200)
+  bank = double('bank', :balance => 1000, :statement => statement)
+  allow(bank).to receive(:withdraw).with(800).and_return(200)
+  allow(bank).to receive(:deposit).with(12000).and_return(12200)
+  allow(bank).to receive(:withdraw).with(5000).and_return(7200)
+  transactions = [transaction1, transaction2, transaction3]
+  statement.instance_variable_set(:@transactions, transactions)
+end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,19 +4,14 @@ require 'helper_methods'
 describe Statement do
   alias_method :statement, :subject
   bank = Bank.new
-
+  let(:bank_double) {double 'bank'}
 
   context 'when there have been no transactions' do
     it "returns 'date || credit || debit || balance'" do
       expect(statement.print_statement).to eq 'date || credit || debit || balance'
     end
   end
-  describe 'statement' do
-    it 'stores transactions in an array' do
-      bank_with_deposit
-      expect(statement.transactions.first).to be_an Transaction
-    end
-  end
+
   context 'when there has been one transaction with a credit' do
     it 'prints a statement containing a date from a previous deposit' do
       bank_with_deposit
@@ -46,8 +41,8 @@ describe Statement do
     end
   end
   context 'when there have been three transactions' do
-    it 'correctly prints a statement containing all transactions' do
-      bank_with_3_transactions
+    it 'correctly prints a statement containing all transactions (stubbed)' do
+      stubbed_bank_3_transactions
       expect(statement.print_statement).to eq "date || credit || debit || balance\n#{Time.now.strftime("%Y-%m-%d")} || || 800.00 || 200.00\n#{Time.now.strftime("%Y-%m-%d")} || 12000.00 || || 12200.00\n#{Time.now.strftime("%Y-%m-%d")} || || 5000.00 || 7200.00"
     end
   end
